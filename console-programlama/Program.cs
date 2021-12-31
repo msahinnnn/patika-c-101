49f839a9622dbbcad46121ff4c19266ac69e0e32
Mehmet Şahin <74006511+msahinnnn@users.noreply.github.com>
2021-12-31 06:58:33 +0300

    Add files via upload

---
+++ console-programlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi girin:");
+            String name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi girin:");
+            String surName = Console.ReadLine();
+
+
+            Console.WriteLine("Merhaba " + name + " " + surName);
+        }
+    }
+}
